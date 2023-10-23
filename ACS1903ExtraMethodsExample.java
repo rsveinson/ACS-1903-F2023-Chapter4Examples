66825f04765ba275922b2b959eca7ffb4522fec9
rsveinson <rsveinson@mts.net>
2023-10-23 11:41:09 -0500

    added another example starter file

---
+++ ACS1903ExtraMethodsExample.java
@@ -0,0 +1,41 @@
+import java.util.Scanner;
+/** 
+ * ACS-1903 Extra methods example
+ * @Sveinson
+ * 
+ * Description: Using methods and functional decomposition
+ *              get two numbers from the keyboard and compute
+ *              the sum of the numeric sequence from low to high.
+*/
+
+public class ACS1903ExtraMethodsExample{
+    public static void main(String[] args) {
+    // **** variables and constants ***
+    
+        int n1 = 0;
+        int n2 = 0;
+        
+        int sum = 0;
+    
+    // **** objects ****
+        Scanner scanner = new Scanner(System.in);
+        
+    // **** input ****
+    
+        // get n1
+        
+        // get n2
+    
+    // **** calculations ****
+    
+        // get the sum of the sequence low to high
+    
+    // **** output ****
+    
+        // print the results of the computation
+    
+    // *** end of program ****
+        System.out.println("end of program");
+    }// end main
+    
+}// end class
